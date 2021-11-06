715f84599d557b0adfbc9ffa8ecb591541e39888
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2021-11-06 11:08:23 +0500

    Changed the image of planet besides the order form

--- src/components/BookTicket.tsx
+++ src/components/BookTicket.tsx
@@ -3,7 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from "@material-ui/core/Button";
 
-import planetImage from "../images/planet-image-new.png";
+import planetImage from "../images/planet2.png";
 
 
 const BookTicket = (): JSX.Element => {
